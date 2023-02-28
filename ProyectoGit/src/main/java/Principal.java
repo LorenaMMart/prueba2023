6dc58b805a9230eab278e62980ae491a2b6ec826
alumno <alumno@30010577F138B8>
2023-02-28 14:12:20 +0100

    Mensaje en el principal

--- ProyectoGit/src/main/java/Principal.java
+++ ProyectoGit/src/main/java/Principal.java
@@ -14,6 +14,8 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Esto es oto cambio realizado");
         // TODO code application logic here
     }
     
